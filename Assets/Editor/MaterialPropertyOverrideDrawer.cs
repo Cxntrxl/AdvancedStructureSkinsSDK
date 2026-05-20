8055f9ca3d54da50ec4f3933f2ea72fc67e84a2d
Cxntrxl <jacksonwood2905@gmail.com>
2026-05-20 16:35:02 +1000

    SDK v1.2.2
    Pushing final changes before updating SDK to v2.0..0

--- Assets/Editor/MaterialPropertyOverrideDrawer.cs
+++ Assets/Editor/MaterialPropertyOverrideDrawer.cs
@@ -1,6 +1,5 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.Rendering;
 
 [CustomPropertyDrawer(typeof(MaterialPropertyOverride))]
--- Assets/Editor/MeshSkinBuilderWindow.cs
+++ Assets/Editor/MeshSkinBuilderWindow.cs
@@ -1,7 +1,5 @@
 using System;
 using UnityEngine;
-
-using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.IO;
